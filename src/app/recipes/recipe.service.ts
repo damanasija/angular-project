085a41066b5cac4fe6594b7c195989d1497b23eb
Damanjeet Asija <daman.asija@gmail.com>
2018-06-21 21:59:50 +0530

    ingredients can be seen in recipe detail, recipe model changed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,30 @@
 import { EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model'
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
 
   public recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe("Test Recipe", "this is simply a test", "https://i.ytimg.com/vi/vHUf7mNM4s8/hqdefault.jpg"),
-    new Recipe("Another Test Recipe", "this is simply a test", "https://i.ytimg.com/vi/vHUf7mNM4s8/hqdefault.jpg"),
+    new Recipe(
+      "Creamy Doughnut",
+      "A sweet doughnut just for sweet you!",
+      "http://www.aimadeitforyou.com/wp-content/uploads/2016/04/IMG_9564-1024x683.jpg",
+      [
+        new Ingredient('Whole Milk', 1),
+        new Ingredient('Bread Flour cups', 2)
+      ]
+    ),
+    new Recipe(
+      "Bad-ass Burger",
+      "This creamy monster is appetizing",
+      "https://media.mnn.com/assets/images/2017/06/sonic_mushroom_beef_burger.jpg.653x0_q80_crop-smart.jpg",
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Patty', 1),
+        new Ingredient('Cheese Slices', 2)
+      ]
+    ),
   ];
 
   getRecipes(): Recipe[] {
