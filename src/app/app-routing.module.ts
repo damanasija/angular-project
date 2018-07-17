ff9f39f41ec98dce58c3ab4462ed272c27e2f90b
Damanjeet Asija <daman.asija@gmail.com>
2018-07-18 02:12:32 +0530

    navigating programatically

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 
@@ -14,7 +15,9 @@ const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent, pathMatch: 'full'},
-    {path: ':id', component: RecipeDetailComponent}
+    {path: 'new', component: RecipeEditComponent},    
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent, children: [
     // {path: 'edit', component: ShoppingEditComponent}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,6 +5,7 @@ import {
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -15,11 +16,13 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
 
-
+  onNewRecipe(){
+    this.router.navigate(['/recipes/new']);
+  }
 }
